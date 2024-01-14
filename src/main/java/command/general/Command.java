7c8559426ce94fdd202bca0b4b5698b02ac1704c
Cyprian <CyprianLenoir@gmail.com>
2024-01-14 22:43:36 +0100

    bug fix

--- src/main/java/command/general/Command.java
+++ src/main/java/command/general/Command.java
@@ -8,63 +8,89 @@ import java.util.*;
 import java.util.logging.Logger;
 
 public class Command implements CommandExecutor {
-    public static final List<Argument> arguments = new ArrayList<>() {{
-        add(new Argument(
-                (sender, args, value, index, values) -> value.equalsIgnoreCase("testCommand"),
-                (sender, args, value, index, values) -> true,
-                null,
-                null,
-                null,
-                (sender, args, value, index, values) -> value,
-                null,
-                "label",
-                new ArrayList<>(){{
-                    add(new Argument(
-                            (sender, args, value, index, values) -> value.equalsIgnoreCase("-g"),
-                            (sender, args, value, index, values) -> new ArrayList<>() {{add("-g");}},
-                            (sender, args, value, index, values) -> value,
-                            "global"
-                    ));
-                    add(new Argument(
-                            (sender, args, value, index, values) -> value.equalsIgnoreCase("cool"),
-                            null,
-                            (sender, args, value, index, values) -> new ArrayList<>() {{add("cool");}},
-                            null,
-                            (sender, args, value, index, values) -> Bukkit.broadcastMessage("where cool ?"),
-                            (sender, args, value, index, values) -> value,
-                            (sender, args, values) -> {
-                                boolean global = (boolean) values.get("global");
-                                if (global) Bukkit.broadcastMessage("Global!");
-                                if (global) Bukkit.broadcastMessage("Succes! (1)");
-                            },
-                            "cool",
-                            null
-                    ));
-                    add(new Argument(
-                            (sender, args, value, index, values) -> value.equalsIgnoreCase("Coolsta!"),
-                            null,
-                            (sender, args, value, index, values) -> new ArrayList<>() {{add("cool");}},
-                            null,
-                            null,
-                            (sender, args, value, index, values) -> value,
-                            (sender, args, values) -> {
-                                boolean global = (boolean) values.get("global");
-                                if (global) Bukkit.broadcastMessage("Global!");
-                                if (global) Bukkit.broadcastMessage("Succes! (2)");
-                            },
-                            "coolsta",
-                            null
-                    ));
-                }}
-        ));
-    }};
+    public static List<Argument> getArguments() {
+        return new ArrayList<>() {{
+            add(new Argument(
+                    (sender, args, value, index, values) -> value.equalsIgnoreCase("testCommand"),
+                    (sender, args, value, index, values) -> true,
+                    null,
+                    null,
+                    null,
+                    (sender, args, value, index, values) -> value,
+                    null,
+                    "label",
+                    new ArrayList<>(){{
+                        add(new Argument(
+                                (sender, args, value, index, values) -> value.equalsIgnoreCase("-g"),
+                                (sender, args, value, index, values) -> new ArrayList<>() {{add("-g");}},
+                                (sender, args, value, index, values) -> true,
+                                "global"
+                        ));
+                        add(new Argument(
+                                (sender, args, value, index, values) -> value.equalsIgnoreCase("cool"),
+                                null,
+                                (sender, args, value, index, values) -> new ArrayList<>() {{add("cool");}},
+                                null,
+                                (sender, args, value, index, values) -> Bukkit.broadcastMessage("where cool ?"),
+                                (sender, args, value, index, values) -> value,
+                                (sender, args, values) -> {
+                                    boolean global = (boolean) values.get("global");
+                                    if (global) Bukkit.broadcastMessage("Global!");
+                                    Bukkit.broadcastMessage("Succes! (1)");
+                                },
+                                "cool",
+                                null
+                        ));
+                        add(new Argument(
+                                (sender, args, value, index, values) -> value.equalsIgnoreCase("Coolsta!"),
+                                null,
+                                (sender, args, value, index, values) -> new ArrayList<>() {{add("Coolsta!");}},
+                                null,
+                                null,
+                                (sender, args, value, index, values) -> value,
+                                (sender, args, values) -> {
+                                    boolean global = (boolean) values.get("global");
+                                    if (global) Bukkit.broadcastMessage("Global!");
+                                    Bukkit.broadcastMessage("Succes! (2)");
+                                },
+                                "coolsta",
+                                null
+                        ));
+                        add(new Argument(
+                                (sender, args, value, index, values) -> value.equalsIgnoreCase("lol"),
+                                null,
+                                (sender, args, value, index, values) -> new ArrayList<>() {{add("lol");}},
+                                null,
+                                null,
+                                (sender, args, value, index, values) -> value,
+                                null,
+                                "lol",
+                                new ArrayList<>(){{
+                                    add(new Argument(
+                                            (sender, args, value, index, values) -> value.equalsIgnoreCase("cyp"),
+                                            null,
+                                            (sender, args, value, index, values) -> new ArrayList<>() {{add("cyp");}},
+                                            null,
+                                            (sender, args, value, index, values) -> Bukkit.broadcastMessage("where cyp?"),
+                                            (sender, args, value, index, values) -> value,
+                                            (sender, args, values) -> Bukkit.broadcastMessage("Cyp"),
+                                            "cyp",
+                                            null
+                                    ));
+                                }}
+                        ));
+                    }}
+            ));
+        }};
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         List<String> list = new LinkedList<>(Arrays.asList(args));
         list.add(0, label);
         final String[] finalArgs = list.toArray(new String[list.size()]);
 
-        List<Argument> argumentsCopy = arguments;
+        List<Argument> argumentsCopy = getArguments();
 
         HashMap<String, Object> values = new HashMap<>();
         for (int i = 0; i < finalArgs.length; i++) {
@@ -80,22 +106,29 @@ public class Command implements CommandExecutor {
 
                 if (currentArgument.isValid == null || currentArgument.isValid.test(sender, finalArgs, arg, finalI, values)){
                     values.put(currentArgument.key,currentArgument.argumentHandler.apply(sender, finalArgs, arg, finalI, values));
+
                     argumentsCopy.stream().filter((a) -> a.modifier).forEach((ma) -> {
-                        if (!values.containsKey(ma.key)) values.put(ma.key, false);
+                        values.putIfAbsent(ma.key, false);
                     });
 
                     if (i+1 == finalArgs.length) {
                         if (currentArgument.invoke != null) {
                             currentArgument.invoke.accept(sender, finalArgs, values);
                         } else {
-                            List<Argument> inferiorArguments = currentArgument.followingArguments;
+                            List<Argument> inferiorArguments;
+                            if (currentArgument.modifier) {
+                                inferiorArguments = argumentsCopy;
+                                inferiorArguments.remove(currentArgument);
+                            } else {
+                                inferiorArguments = currentArgument.followingArguments;
+                            }
                             Argument firstInferiorArgument = inferiorArguments.stream()
                                     .filter(obj -> obj.errorMissing != null)
                                     .findFirst().get();
 
                             firstInferiorArgument.errorMissing.accept(sender, finalArgs, arg, i, values);
-                            return false;
                         }
+                        return false;
                     } else {
                         if (currentArgument.modifier) {
                             argumentsCopy.remove(currentArgument);
--- src/main/java/command/general/Completer.java
+++ src/main/java/command/general/Completer.java
@@ -6,49 +6,59 @@ import org.bukkit.command.TabCompleter;
 
 import java.util.*;
 
-import static command.general.Command.arguments;
-
 public class Completer implements TabCompleter {
+    static final List<String> emptyResult = new ArrayList<>();
     @Override
     public List<String> onTabComplete(CommandSender sender, Command commands, String label, String[] args) {
         List<String> list = new LinkedList<>(Arrays.asList(args));
         list.add(0, label);
         final String[] finalArgs = list.toArray(new String[list.size()]);
 
-        List<Argument> argumentsCopy = arguments;
+        List<Argument> argumentsCopy = command.general.Command.getArguments();
 
         HashMap<String, Object> values = new HashMap<>();
         for (int i = 0; i < finalArgs.length; i++) {
             final String arg = finalArgs[i];
             final int finalI = i;
 
-            Optional<Argument> currentOptionalArgument = argumentsCopy.stream()
-                    .filter(obj -> obj.isArgument.test(sender, finalArgs, arg, finalI, values))
-                    .findFirst();
+            if (finalI+1 == finalArgs.length) {
+                List<String> tabCompletions = new ArrayList<>();
+                argumentsCopy.forEach((obj) -> tabCompletions.addAll(obj.tabCompletions.apply(sender, finalArgs, arg, finalI, values)));
 
-            if (currentOptionalArgument.isEmpty()) return null;
+                int argEnd = finalArgs.length - 1;
+                List<String> result = new ArrayList<>();
+                for (String tabCompletion : tabCompletions) {
+                    if (tabCompletion.toLowerCase().startsWith(finalArgs[argEnd].toLowerCase())) {
+                        result.add(tabCompletion);
+                    }
+                }
 
-            Argument currentArgument = currentOptionalArgument.get();
+                return result;
+            } else{
+                Optional<Argument> currentOptionalArgument = argumentsCopy.stream()
+                        .filter(obj -> obj.isArgument.test(sender, finalArgs, arg, finalI, values))
+                        .findFirst();
 
-            if (!(currentArgument.isValid == null || currentArgument.isValid.test(sender, finalArgs, arg, finalI, values))) return null;
+                if (currentOptionalArgument.isEmpty()) return emptyResult;
 
-            values.put(currentArgument.key, currentArgument.argumentHandler.apply(sender, finalArgs, arg, finalI, values));
-            argumentsCopy.stream().filter((a) -> a.modifier).forEach((ma) -> {
-                if (!values.containsKey(ma.key)) values.put(ma.key, false);
-            });
+                Argument currentArgument = currentOptionalArgument.get();
 
-            if (i+1 == finalArgs.length) {
-                return currentArgument.tabCompletions.apply(sender, finalArgs, arg, finalI, values);
-            } else {
+                if (!(currentArgument.isValid == null || currentArgument.isValid.test(sender, finalArgs, arg, finalI, values))) return emptyResult;
+
+                values.put(currentArgument.key, currentArgument.argumentHandler.apply(sender, finalArgs, arg, finalI, values));
+                argumentsCopy.stream().filter((a) -> a.modifier).forEach((ma) -> {
+                    if (!values.containsKey(ma.key)) values.put(ma.key, false);
+                });
+
                 if (currentArgument.modifier) {
                     argumentsCopy.remove(currentArgument);
                 } else {
                     argumentsCopy = currentArgument.followingArguments;
+                    if (argumentsCopy == null) return emptyResult;
                 }
             }
-
         }
 
-        return null;
+        return emptyResult;
     }
 }
